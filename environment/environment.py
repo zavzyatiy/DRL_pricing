53f35567f5d9a84cf3ceea1d1939e6fe94b59525
Roman Ermolchuk <kabinnet.2308@gmail.com>
2025-02-24 14:07:33 +0300

    Thinking...

--- environment/environment.py
+++ environment/environment.py
@@ -1,6 +1,14 @@
 ### начальные условия: инициализация весов для всех алгоритмов
+
+import random as rnd
+import numpy as np
+import torch
+
 ### иницализация random.seed
+
+rnd.seed(42)
+
 ### инициализация гиперпараметров: n, m, \delta, \gamma,
 ### c_i, h^+, v^-, \eta
 
-
+print(rnd.randint(1, 10))
