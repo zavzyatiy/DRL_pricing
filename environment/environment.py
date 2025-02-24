40203f0fbf426ee6df876a1de98e988c80dbd8e4
Roman Ermolchuk <kabinnet.2308@gmail.com>
2025-02-24 15:09:39 +0300

    Sync option for venv

--- environment/environment.py
+++ environment/environment.py
@@ -1,4 +1,6 @@
 ### начальные условия: инициализация весов для всех алгоритмов
+### для env: & ".\.venv\Scripts\Activate"
+### для папки: cd DRL_pricing
 
 import random as rnd
 import numpy as np
@@ -7,6 +9,7 @@ import torch
 ### иницализация random.seed
 
 rnd.seed(42)
+torch.manual_seed(42)
 
 ### инициализация гиперпараметров: n, m, \delta, \gamma,
 ### c_i, h^+, v^-, \eta
