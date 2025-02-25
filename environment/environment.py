15fc96c88ccd3f91f25b5ec0b84c4f27832dae18
Roman Ermolchuk <kabinnet.2308@gmail.com>
2025-02-25 15:43:58 +0300

    working

--- environment/environment.py
+++ environment/environment.py
@@ -5,6 +5,13 @@
 import random as rnd
 import numpy as np
 import torch
+
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
 from firms.firms import epsilon_greedy
 
 ### иницализация randomseed
