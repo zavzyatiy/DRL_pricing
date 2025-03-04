Critical bug fixed in the method of invalid actions masking

environment/environment.py
@@ -66,6 +66,8 @@ VISUALIZE = Environment["VISUALIZE"]
 VISUALIZE_THEORY = Environment["VISUALIZE_THEORY"]
 # Сохранить итоговоый график?
 SAVE = Environment["SAVE"]
+# Выводить итоговую информацию о симуляциях?
+SUMMARY = Environment["SUMMARY"]
 # С какой стороны отображать легенду на графиках
 loc = Environment["loc"]
 
@@ -105,9 +107,6 @@ for env in range(ENV):
         raw_stock_history = []
 
     ### Инициализация однородных фирм
-
-    # M = Environment["firm_model"]
-    # firm_params = Environment["firm_params"]
 
     firms = [deepcopy(M(**firm_params)) for i in range(n)]
 
@@ -237,21 +236,25 @@ for env in range(ENV):
             else:
                 learn = learn[1:] + [[x[1] for x in idxs]]
             
-            inv = []
-            p = []
-            for i in range(n):
-                if inventory[idxs[i][0]] >= x_t[i]:
-                    inv.append(inventory[idxs[i][0]])
-                else:
-                    inv.append(x_t[i])
+            inv = [inventory[x[0]] for x in idxs]
+            p = [prices[x[1]] for x in idxs]
+            # inv = []
+            # p = []
+            # for i in range(n):
+            #     # if inventory[idxs[i][0]] >= x_t[i]:
+            #     #     inv.append(inventory[idxs[i][0]])
+            #     # else:
+            #     #     print("!!!")
+            #     #     inv.append(x_t[i])
                 
-                p.append(prices[idxs[i][1]])
+            #     p.append(prices[idxs[i][1]])
+            #     inv.append(inventory[idxs[i][0]])
 
             doli = spros.distribution(p)
 
             pi = []
             for i in range(n):
-                pi_i = p[i] * doli[i] - c_i * (inv[i] - x_t[i]) - h_plus * max(0, inv[i] - doli[i]) - v_minus * min(0, inv[i] - doli[i])
+                pi_i = p[i] * doli[i] - c_i * (inv[i] - x_t[i]) - h_plus * max(0, inv[i] - doli[i]) - v_minus * min(0, doli[i] - inv[i])
                 pi.append(pi_i)
 
             if len(learn) == MEMORY_VOLUME:
@@ -440,32 +443,37 @@ if VISUALIZE or SAVE:
         plt.show()
 
 
-Price_history = np.array(Price_history)
-Profit_history = np.array(Profit_history)
-# print(Price_history)
-# print(Profit_history)
+if SUMMARY:
+    Price_history = np.array(Price_history)
+    Profit_history = np.array(Profit_history)
+    # print(Price_history)
+    # print(Profit_history)
 
-print(f"Средняя цена по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Price_history[:, i]), 3)) for i in range(n)]))
-print(f"Средняя прибыль по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Profit_history[:, i]), 3)) for i in range(n)]))
+    print(f"Средняя цена по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Price_history[:, i]), 3)) for i in range(n)]))
 
-if TN_DDQN == 1:
-    Stock_history = np.array(Stock_history)
-    # print(Stock_history)
-    print(f"Среднии запасы по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Stock_history[:, i]), 3)) for i in range(n)]))
+    if TN_DDQN == 1:
+        Stock_history = np.array(Stock_history)
+        # print(Stock_history)
+        print(f"Среднии запасы по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Stock_history[:, i]), 3)) for i in range(n)]))
 
-print("-"*20*n)
-print("Теоретические цены:", round(p_NE , 3), round(p_M , 3))
-print("Теоретические прибыли:", round(pi_NE , 3), round(pi_M , 3))
+    print(f"Средняя прибыль по последним {int(T/20)} раундов:", " ".join([str(round(np.mean(Profit_history[:, i]), 3)) for i in range(n)]))
 
-if TN_DDQN == 1:
-    print("Теоретические инв. в запасы:", round(inv_NE , 3), round(inv_M , 3))
+    print("-"*20*n)
+    print("Теоретические цены:", round(p_NE , 3), round(p_M , 3))
 
-print("-"*20*n)
-print("Индекс сговора по цене:", str(round(100 * (np.mean(Price_history) - p_NE)/(p_M - p_NE), 2)) + "%")
-print("Индекс сговора по прибыли:", str(round(100 * (np.mean(Profit_history) - pi_NE)/(pi_M - pi_NE), 2)) + "%")
+    if TN_DDQN == 1:
+        print("Теоретические инв. в запасы:", round(inv_NE , 3), round(inv_M , 3))
 
-if TN_DDQN == 1:
-    print("Индекс сговора по запасам:", str(round(100 * (np.mean(Stock_history) - inv_NE)/(inv_M - inv_NE), 2)) + "%")
+    print("Теоретические прибыли:", round(pi_NE , 3), round(pi_M , 3))
+
+    print("-"*20*n)
+    print("Индекс сговора по цене:", str(round(100 * (np.mean(Price_history) - p_NE)/(p_M - p_NE), 2)) + "%")
+
+    if TN_DDQN == 1:
+        print("Индекс сговора по запасам:", str(round(100 * (np.mean(Stock_history) - inv_NE)/(inv_M - inv_NE), 2)) + "%")
+
+    print("Индекс сговора по прибыли:", str(round(100 * (np.mean(Profit_history) - pi_NE)/(pi_M - pi_NE), 2)) + "%")
+
 
 """
 Средняя цена по всем раундам: 1.6830796000000001 1.6826728
@@ -476,28 +484,16 @@ n = 2, ENV = 100, T = 100000, mode = "zhou", MEMORY_VOLUME = 1, own = False
 Средняя прибыль по всем раундам: 0.12345687961002637 0.12364908988203568 0.12292997137728587
 n = 3, eps = 0.9, ENV = 100, T = 100000, mode = "zhou", MEMORY_VOLUME = 1, own = False
 
-T = 10000, ENV = 40
-Средняя цена по всем раундам: 0.926 0.975
-Средняя прибыль по всем раундам: 0.173 0.134
-Среднии запасы по всем раундам: 0.476 0.466
+T = 10000, ENV = 30, h_plus = 1.17498, v_minus = 1.17498, n = 2, arms = 21 (оба), batch_size = 128
+Средняя цена по последним 500 раундов: 0.858 0.837
+Средняя прибыль по последним 500 раундов: 0.24 0.265
+Среднии запасы по последним 500 раундов: 0.467 0.402
 ----------------------------------------
 Теоретические цены: 0.723 1.175
 Теоретические прибыли: 0.223 0.337
 Теоретические инв. в запасы: 0.471 0.365
 ----------------------------------------
-Индекс сговора по цене: 50.37%
-Индекс сговора по прибыли: -60.34%
-Индекс сговора по запасам: 0.35%
-
-Средняя цена по последним 5000 раундов: 0.954 0.933
-Средняя прибыль по последним 5000 раундов: 0.291 0.304
-Среднии запасы по последним 5000 раундов: 0.244 0.274
-----------------------------------------
-Теоретические цены: 0.723 1.175
-Теоретические прибыли: 0.223 0.337
-Теоретические инв. в запасы: 0.471 0.365
-----------------------------------------
-Индекс сговора по цене: 48.7%
-Индекс сговора по прибыли: 64.98%
-Индекс сговора по запасам: 199.09%
+Индекс сговора по цене: 27.59%
+Индекс сговора по прибыли: 25.81%
+Индекс сговора по запасам: 34.82%
 """

environment/firms_RL.py
@@ -253,10 +253,10 @@ class TN_DDQN:
             state_dim: int,
             inventory_actions: list,
             price_actions: list,
-            batch_size:int,  # =32,
+            batch_size:int,
             MEMORY_VOLUME: int,
-            gamma: float, # =0.95,
-            lr: float, # =0.001,
+            gamma: float,
+            lr: float,
             eps: float,
             mode: str,
             target_update_freq: int,
@@ -273,11 +273,11 @@ class TN_DDQN:
 
         # Q-network
         self.online = DQNet(state_dim, inventory_actions, price_actions)
-        self.target_net = deepcopy(self.online)
+        self.target = deepcopy(self.online)
 
         if cuda_usage:
             self.online = DQNet(state_dim, inventory_actions, price_actions).to(self.device)
-            self.target_net = deepcopy(self.online).to(self.device)
+            self.target = deepcopy(self.online).to(self.device)
         
         # Hyperparameters
         self.batch_size = batch_size
@@ -291,15 +291,17 @@ class TN_DDQN:
         elif mode == "zhou":
             self.eps_min = 0.01
             self.eps_max = 1
-            self.beta = 0.005 # 1.5/(10**4)
+            self.beta = 1.5/(10**4) # 0.005, 1.5/(10**3), 1.5/(10**4)
         
         self.target_update_freq = target_update_freq
         self.memory = deque(maxlen=memory_size)
         
         self.optimizer = torch.optim.Adam(self.online.parameters(), lr=lr)
 
+
     def __repr__(self):
         return "TN_DDQN"    
+
 
     def _get_state_vector(self, firm_state):
         """Преобразует состояние фирмы в тензор"""
@@ -314,7 +316,7 @@ class TN_DDQN:
     def suggest_actions(self, firm_state):
         """Возвращает действия (инвентарь, цену)"""
         if self.t < 0:
-            inv_idx = random.randint(0, len(self.inventory_actions)-1)
+            inv_idx = random.randint(np.sum(self.inventory_actions < firm_state["current_inventory"]), len(self.inventory_actions)-1)
             price_idx = random.randint(0, len(self.price_actions)-1)
             self.t += 1
             return (inv_idx, price_idx)
@@ -322,11 +324,20 @@ class TN_DDQN:
         state = self._get_state_vector(firm_state)
         
         if random.random() < self.eps:
-            inv_idx = random.randint(0, len(self.inventory_actions)-1)
+            inv_idx = random.randint(np.sum(self.inventory_actions < firm_state["current_inventory"]), len(self.inventory_actions)-1)
             price_idx = random.randint(0, len(self.price_actions)-1)
         else:
             with torch.no_grad():
                 inv_q, price_q = self.online(state.unsqueeze(0), 'online')
+
+                if not self.cuda_usage:
+                    inventory_actions_tensor = torch.tensor(self.inventory_actions)
+                else:
+                    inventory_actions_tensor = torch.tensor(self.inventory_actions).to(self.device)
+                
+                mask = inventory_actions_tensor.unsqueeze(0) < state[0]
+                inv_q[mask] = -float('inf')
+
                 inv_idx = torch.argmax(inv_q).item()
                 price_idx = torch.argmax(price_q).item()
         
@@ -390,19 +401,25 @@ class TN_DDQN:
         
         mask = inventory_actions_tensor.unsqueeze(0) < states[:, 0].unsqueeze(1)
         inv_q_online[mask] = -float('inf')
-        
+
         inv_selected = inv_q_online.gather(1, actions[:, 0].unsqueeze(1))
         price_selected = price_q_online.gather(1, actions[:, 1].unsqueeze(1))
 
         # Target Q values (target network)
         with torch.no_grad():
-            inv_q_target, price_q_target = self.target_net(next_states, 'target')
+            inv_q_target, price_q_target = self.target(next_states, 'target')
+            inv_to_max_online, price_to_max_online = self.online(next_states, 'online')
             
             mask_target = inventory_actions_tensor.unsqueeze(0) < next_states[:, 0].unsqueeze(1)
             inv_q_target[mask_target] = -float('inf')
+            inv_to_max_online[mask_target] = -float('inf')
+            
+            inv_argmax = torch.argmax(inv_to_max_online, dim = 1)
+            price_argmax = torch.argmax(price_to_max_online, dim = 1)
+            
+            inv_max = torch.gather(inv_q_target, 1, inv_argmax.unsqueeze(1)).squeeze()
+            price_max = torch.gather(price_q_target, 1, price_argmax.unsqueeze(1)).squeeze()
 
-            inv_max = inv_q_target.max(1)[0]
-            price_max = price_q_target.max(1)[0]
             targets = rewards + self.gamma * (inv_max + price_max)
 
         # Loss calculation
@@ -416,7 +433,7 @@ class TN_DDQN:
         self.optimizer.step()
 
         if self.t >= 0 and self.t % self.target_update_freq == 0:
-            self.target_net.load_state_dict(self.online.state_dict())
+            self.target.load_state_dict(self.online.state_dict())
 
         return total_loss.item()
 
@@ -440,28 +457,7 @@ class TN_DDQN:
         """Загрузка модели"""
         checkpoint = torch.load(path)
         self.online.load_state_dict(checkpoint['model'])
-        self.target_net.load_state_dict(checkpoint['model'])
+        self.target.load_state_dict(checkpoint['model'])
         self.eps = checkpoint.get('exploration', 1.0)
         self.memory = checkpoint.get('memory', deque(maxlen=self.memory_size))
 
-#### ЭТО НУЖНО СДЕЛАТЬ!
-# class TN_DDQN:
-     
-#     def __init__(
-#             self,
-            
-# 			):
-
-#         pass
-
-#     def __repr__(self):
-#         return "TN_DDQN"
-
-#     def adjust_memory(self, ):
-#         pass
-
-#     def suggest(self):
-#         pass
-
-#     def update(self, ):
-#         pass